a23448131bd27f4fdd91a2ba8acc9c29bc8271c0
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-06-15 18:09:59 +0530

    Time: 11 ms (5.68%), Space: 10.7 MB (33.46%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-
-    vector<vector<int>> recursion(vector<vector<int>>& ans, vector<int>& temp, int index, int n, vector<int>& arr){
-        
-        if(index == n){
-            ans.push_back(temp);
-            return ans;
+    void allset(int index,vector<int>& nums,vector<vector<int>>& ans,vector<int>& ds){
+        if(index==nums.size()){
+            ans.push_back(ds);
+            return;
         }
-
-
-        temp.push_back(arr[index]);
-        recursion(ans,temp,index+1,n,arr);
-        temp.pop_back();
-        recursion(ans,temp,index+1,n,arr);
-        return ans;
-
+        
+        allset(index+1,nums,ans,ds);
+        ds.push_back(nums[index]);
+        allset(index+1,nums,ans,ds);
+        ds.pop_back();
     }
-
+    
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        int n = nums.size();
-        return recursion(ans,temp,0,n,nums);
+        vector<int> ds;
+        allset(0,nums,ans,ds);
+        
+        return ans;
     }
 };
