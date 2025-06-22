cf952e493f237bae6d0acd0f1c3c9fe70999edbf
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-06-22 15:53:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
 
-    int recursion(vector<int> & dp, int steps) {
+    // int recursion(vector<int> & dp, int steps) {
         
-        if(steps < 0) return 0;
-        if(steps == 0) return 1;
+    //     if(steps < 0) return 0;
+    //     if(steps == 0) return 1;
 
-        if(dp[steps] != -1) return dp[steps];
-        dp[steps] = recursion(dp,steps-1) + recursion(dp,steps-2);
-        return dp[steps];
-    }
+    //     if(dp[steps] != -1) return dp[steps];
+    //     dp[steps] = recursion(dp,steps-1) + recursion(dp,steps-2);
+    //     return dp[steps];
+    // }
 
     int climbStairs(int steps) {
         vector<int> dp(steps+1,-1);
-        return recursion(dp,steps);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2;i<steps+1;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[steps];
     }
 };
