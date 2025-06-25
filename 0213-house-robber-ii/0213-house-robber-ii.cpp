ec2448bb1b8bde6b51d7ec341031fc18f71686da
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-06-25 21:00:27 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.1%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    int recursion(vector<int>& dp, int n, vector<int>& nums) {    
-        if(n < 0) return 0;
-        if(n == 0) return nums[n];
+    // int recursion(vector<int>& dp, int n, vector<int>& nums) {    
+    //     if(n < 0) return 0;
+    //     if(n == 0) return nums[n];
 
-        if(dp[n] != -1) return dp[n];
-        int take = nums[n] + recursion(dp,n-2,nums);
-        int nontake = recursion(dp,n-1,nums);
+    //     if(dp[n] != -1) return dp[n];
+    //     int take = nums[n] + recursion(dp,n-2,nums);
+    //     int nontake = recursion(dp,n-1,nums);
 
-        dp[n] = max(take,nontake);
-        return dp[n];
+    //     dp[n] = max(take,nontake);
+    //     return dp[n];
+    // }
+
+    int tabulation(int start, int end, vector<int>& nums){
+        int n = end-start+1;
+        if (n == 0) return 0;
+        if (n == 1) return nums[start];
+
+
+        vector<int>tab(n+1,0);
+        tab[0] = nums[start];
+        tab[1] = max(nums[start],nums[start+1]);
+
+        for(int i=2;i<n;i++){
+            tab[i] = max(tab[i - 1], tab[i - 2] + nums[start + i]);
+        }
+
+        return tab[n-1];
     }
 
     int rob(vector<int>& nums) {
@@ -17,11 +34,9 @@ public:
         if(n == 1) return nums[0]; 
         if(n == 2) return max(nums[0],nums[1]);
 
-        vector<int> front(nums.begin(),nums.end()-1);
-        vector<int> end(nums.begin()+1,nums.end());
-        vector<int> dp1(n+1,-1);
-        vector<int> dp2(n+1,-1);
+        int ans = tabulation(0, n-2,nums);
+        int ans1 = tabulation(1, n-1,nums);
 
-        return max(recursion(dp1,n-2,front),recursion(dp2,n-2,end));
+        return max(ans,ans1);
     }
 };
