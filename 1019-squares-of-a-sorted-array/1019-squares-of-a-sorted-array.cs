0f1c2fc36e8a6b50ffe7542d58961d7f4daa0d05
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-07-24 19:38:07 +0530

    Time: 25 ms (38.3%), Space: 65 MB (30.93%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        int n = nums.Length;
+        
+        for(int i=0;i<nums.Length;i++){
+            nums[i] = nums[i]*nums[i];
+        }
+
+        Array.Sort(nums);
+        return nums;
+    }
+}
