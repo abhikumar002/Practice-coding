b6dcc48f5d855c12f846b5700697842aa7762a2d
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-09-01 20:52:11 +0530

    Time: 34 ms (9.33%), Space: 49 MB (91.93%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cs
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cs
@@ -1,15 +1,4 @@
 public class Solution {
-    
-    public int FindMax(int[] piles){
-        int maxi = int.MinValue;;
-        
-        for(int i=0;i<piles.Length;i++){
-            maxi = Math.Max(maxi,piles[i]);
-        }
-        
-        return maxi;
-    }
-    
     public long checkhour(int[] piles, int h){
         long requiretime = 0;
         
@@ -19,23 +8,22 @@ public class Solution {
         
         return requiretime;
     }
-    
+
     public int MinEatingSpeed(int[] piles, int h) {
+        Array.Sort(piles);
         int n = piles.Length;
-        int left = 1;
-        int right = FindMax(piles);
-        int ans = 0;
-        
-        while(left <= right){
-            int hour = (left + right) / 2;
-            long temp = checkhour(piles,hour);
-            
-            if(temp <= h) {
-                right = hour - 1;
-            }
-            else left = hour + 1 ;
+        int low = 1;
+        int high = piles[n-1];
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            //Console.WriteLine(mid + " "+low+" "+high);
+            long totalhour = checkhour(piles,mid);
+            //Console.WriteLine(totalhour);
+            if(totalhour <= h) high = mid-1;
+            else low = mid+1;
         }
-        
-        return left;
+
+        return low;
     }
 }
