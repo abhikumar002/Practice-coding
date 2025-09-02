9cddb3bfddbaf43354c27e43b745f518f2831f44
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-09-02 23:05:10 +0530

    Time: 0 ms (100%), Space: 42.3 MB (61.37%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
@@ -22,3 +22,52 @@ public class Solution {
         return nums[t];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  public int FindMin(int[] nums) {
+//         int n = nums.Length;
+//         if(nums[0] < nums[n-1]) return nums[0];
+
+//         int left = 0;
+//         int right = n-1;
+//         int ans = int.MaxValue;
+//         int t = ans;
+
+//         while(left <= right){
+//             int mid = (left + right)/2;
+//             // Console.WriteLine(mid);
+//             if(ans > nums[mid]){
+//                 ans = nums[mid];
+//                 t = mid;
+//             }
+//             if(nums[mid] < nums[right]) right = mid-1;
+//             else left = mid+1;
+//         }
+
+//         return nums[t];
+//     }
