3af6d9d49b5fe51e5db0a7a005d0539c317bf6d9
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-10-29 10:38:43 +0530

    Time: 858 ms (15.84%), Space: 11.5 MB (50.74%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    
-    bool recursion(int n,int i,int j, vector<vector<char>>& board, string word){
-        
+    bool recursion(int n, int row, int col, vector<vector<char>>& board, string word){
         if(n == word.size()) return true;
-        if(i<0 || i>=board.size() || j<0 || j>=board[i].size() || board[i][j] !=word[n]) return false;
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[row].size() || board[row][col] != word[n]) return false;
 
-        board[i][j] = '0';
-        bool ans = recursion(n+1,i+1,j,board,word) || 
-                   recursion(n+1,i,j+1,board,word) ||
-                   recursion(n+1,i-1,j,board,word) ||
-                   recursion(n+1,i,j-1,board,word);
+        board[row][col] = '0';
+        bool ans = recursion(n+1, row+1, col, board, word) || 
+                   recursion(n+1, row, col+1, board, word) || 
+                   recursion(n+1, row-1, col, board, word) || 
+                   recursion(n+1, row, col-1, board, word);
 
-        board[i][j] = word[n];
+        board[row][col] = word[n];
         return ans;
-        
-    }
+    } 
 
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<int>> ans;
-        int n = board.size();
-        int m = board[0].size();
-
         if(word == "") return false;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(board[i][j] == word[0] && recursion(0,i,j,board,word)) return true;
-            } 
-        } 
+
+        for(int i =0 ;i<board.size();i++){
+            for(int j =0 ;j<board[0].size();j++){
+                if((board[i][j] == word[0]) && recursion(0, i, j, board, word)) return true;
+            }
+        }
+
         return false;
     }
 };
