2693e0a49fb0f6c860ab327f1571ff9331a83df5
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-11-12 10:24:23 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    int recursion(int index, vector<int>& nums, vector<int>& dp) {
-        if(index < 0) return 0;
-        if(index == 0) return nums[index];
+    //Approach 1 : Recursion + DP
+    // int recursion(int index, vector<int>& nums, vector<int>& dp) {
+    //     if(index < 0) return 0;
+    //     if(index == 0) return nums[index];
 
-        if(dp[index] != -1) return dp[index];
-        int take = nums[index] + recursion(index-2, nums, dp);
-        int nontake = recursion(index-1, nums, dp);
+    //     if(dp[index] != -1) return dp[index];
+    //     int take = nums[index] + recursion(index-2, nums, dp);
+    //     int nontake = recursion(index-1, nums, dp);
 
-        dp[index] = max(take, nontake);
-        return dp[index];
-    }
+    //     dp[index] = max(take, nontake);
+    //     return dp[index];
+    // }
 
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n<2) return nums[0];
+    //     vector<int> dp(n+1, -1);
+    //     return max(recursion(n-1, nums, dp),recursion(n-2, nums, dp)) ;
+    // }
+
+    //Approach 2 : Tempo
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n<2) return nums[0];
+        if(n==1) return nums[0];
         vector<int> dp(n+1, -1);
-        return max(recursion(n-1, nums, dp),recursion(n-2, nums, dp)) ;
+
+        dp[0] = nums[0];
+        dp[1] = max( nums[0], nums[1]);
+
+        for(int i=2; i<n;i++){
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
