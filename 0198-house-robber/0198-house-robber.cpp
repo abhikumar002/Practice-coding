930a0c3cccd99b9f56225848369b248208e89ccf
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-11-12 10:19:24 +0530

    Time: 3 ms (6.15%), Space: 10.7 MB (46.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    // int recursion(vector<int>& dp, int n, vector<int>& nums) {
-        
-    //     if(n < 0) return 0;
-    //     if(n == 0) return nums[n];
+    int recursion(int index, vector<int>& nums, vector<int>& dp) {
+        if(index < 0) return 0;
+        if(index == 0) return nums[index];
 
-    //     if(dp[n] != -1) return dp[n];
-    //     int take = nums[n] + recursion(dp,n-2,nums);
-    //     int nontake = recursion(dp,n-1,nums);
+        if(dp[index] != -1) return dp[index];
+        int take = nums[index] + recursion(index-2, nums, dp);
+        int nontake = recursion(index-1, nums, dp);
 
-    //     dp[n] = max(take,nontake);
-    //     return dp[n];
-    // }
+        dp[index] = max(take, nontake);
+        return dp[index];
+    }
 
     int rob(vector<int>& nums) {
-        // 1 2 3 1
         int n = nums.size();
-        if(n==1) return nums[0];
-        vector<int> tempo(n+1,0);
-        tempo[0] = nums[0];
-        tempo[1] = max(nums[1],nums[0]);
-        int ans = INT_MIN;
-
-        for(int i=2;i<n;i++){
-           tempo[i] = max(nums[i] + tempo[i-2],tempo[i-1]);
-        }
-        return tempo[n-1];
+        if(n<2) return nums[0];
+        vector<int> dp(n+1, -1);
+        return max(recursion(n-1, nums, dp),recursion(n-2, nums, dp)) ;
     }
 };
