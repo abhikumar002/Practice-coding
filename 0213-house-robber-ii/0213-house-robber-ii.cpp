607c114716b3b05ffbb6156847f1d546a2a39b8a
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-11-13 10:21:36 +0530

    Time: 0 ms (100%), Space: 10.9 MB (39.9%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    // int recursion(vector<int>& dp, int n, vector<int>& nums) {    
-    //     if(n < 0) return 0;
-    //     if(n == 0) return nums[n];
 
-    //     if(dp[n] != -1) return dp[n];
-    //     int take = nums[n] + recursion(dp,n-2,nums);
-    //     int nontake = recursion(dp,n-1,nums);
+    //Approach 1 : Recursion + DP
+    int recursion(int left,int right, int index, vector<int>& nums, vector<int>& dp) {
+        if(index < left) return 0;
 
-    //     dp[n] = max(take,nontake);
-    //     return dp[n];
-    // }
+        if(dp[index] != -1) return dp[index];
+        int take = nums[index] + recursion(left, right, index-2, nums, dp);       
+        int nontake = recursion(left, right, index-1, nums, dp);
 
-    int tabulation(int start, int end, vector<int>& nums){
-        int n = end-start+1;
-        if (n == 0) return 0;
-        if (n == 1) return nums[start];
-
-
-        vector<int>tab(n+1,0);
-        tab[0] = nums[start];
-        tab[1] = max(nums[start],nums[start+1]);
-
-        for(int i=2;i<n;i++){
-            tab[i] = max(tab[i - 1], tab[i - 2] + nums[start + i]);
-        }
-
-        return tab[n-1];
+        dp[index] = max(take, nontake);
+        return dp[index];
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0]; 
-        if(n == 2) return max(nums[0],nums[1]);
+        if(n<2) return nums[0];
+        vector<int> dpA(n+1, -1);
+        int caseA = recursion(1, n-1, n-1, nums, dpA);
 
-        int ans = tabulation(0, n-2,nums);
-        int ans1 = tabulation(1, n-1,nums);
+        vector<int> dpB(n+1, -1);
+        int caseB = recursion(0, n-2, n-2, nums, dpB);
 
-        return max(ans,ans1);
+        return max(caseA, caseB);
+
     }
 };
