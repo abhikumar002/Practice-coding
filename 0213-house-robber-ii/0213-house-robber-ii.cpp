0a6d73a3a9e78f61b4538678a0d0d60436e19f81
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-11-13 10:25:24 +0530

    Time: 0 ms (100%), Space: 10.3 MB (76.64%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,27 +2,53 @@ class Solution {
 public:
 
     //Approach 1 : Recursion + DP
-    int recursion(int left,int right, int index, vector<int>& nums, vector<int>& dp) {
-        if(index < left) return 0;
+    // int recursion(int left,int right, int index, vector<int>& nums, vector<int>& dp) {
+    //     if(index < left) return 0;
 
-        if(dp[index] != -1) return dp[index];
-        int take = nums[index] + recursion(left, right, index-2, nums, dp);       
-        int nontake = recursion(left, right, index-1, nums, dp);
+    //     if(dp[index] != -1) return dp[index];
+    //     int take = nums[index] + recursion(left, right, index-2, nums, dp);       
+    //     int nontake = recursion(left, right, index-1, nums, dp);
 
-        dp[index] = max(take, nontake);
-        return dp[index];
+    //     dp[index] = max(take, nontake);
+    //     return dp[index];
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n<2) return nums[0];
+    //     vector<int> dpA(n+1, -1);
+    //     int caseA = recursion(1, n-1, n-1, nums, dpA);
+
+    //     vector<int> dpB(n+1, -1);
+    //     int caseB = recursion(0, n-2, n-2, nums, dpB);
+
+    //     return max(caseA, caseB);
+
+    // }
+
+
+    int robLinear(vector<int>& nums, int l, int r) {
+        int prev = 0, prev2 = 0;
+        for(int i = l; i <= r; i++){
+            int take = nums[i] + prev2;
+            int nottake = prev;
+            int curr = max(take, nottake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n<2) return nums[0];
-        vector<int> dpA(n+1, -1);
-        int caseA = recursion(1, n-1, n-1, nums, dpA);
+        if(n == 1) return nums[0];
 
-        vector<int> dpB(n+1, -1);
-        int caseB = recursion(0, n-2, n-2, nums, dpB);
+        // Case 1: exclude last house
+        int case1 = robLinear(nums, 0, n-2);
 
-        return max(caseA, caseB);
+        // Case 2: exclude first house
+        int case2 = robLinear(nums, 1, n-1);
 
+        return max(case1, case2);
     }
 };
