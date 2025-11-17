0197c6ef0e825dd868b3840374d43f13d6ec614f
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2025-11-17 09:55:26 +0530

    Time: 0 ms (100%), Space: 14.5 MB (19.26%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-
-    void recursion(int index, vector<int>& candidates, set<vector<int>>& hset, vector<int>& temp, int target){
-        if(target == 0) { hset.insert(temp); return; }
-        if(index >= candidates.size() || target < 0) return;
-
-        if(candidates[index]<=target){
-            temp.push_back(candidates[index]);
-            recursion(index+1,candidates, hset, temp, target - candidates[index]); // take
-            temp.pop_back();
+    void SolveSum(int index, vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& temp) {
+        if(target == 0) { 
+            ans.push_back(temp);
+            return;
         }
+        if(target < 0 || index >= candidates.size()) return;
 
-        int nextIndex = index + 1;
-        while (nextIndex < candidates.size() && candidates[nextIndex] == candidates[index]) {
+        //lena hai
+        temp.push_back(candidates[index]);
+        SolveSum(index+1, candidates, target-candidates[index], ans, temp);
+        temp.pop_back();
+
+        int nextIndex = index+1;
+        while(nextIndex < candidates.size() && candidates[nextIndex] == candidates[index]){
             nextIndex++;
         }
-        recursion(nextIndex,candidates, hset, temp, target); // non-take
 
-        return;
+        //nhi lena
+        SolveSum(nextIndex, candidates, target, ans, temp);
+
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        int n = candidates.size();
-        vector<int> temp;
-        set<vector<int>> hset;
         sort(candidates.begin(),candidates.end());
+        vector<vector<int>> duplicates;
+        vector<int> temp;
 
-        recursion(0,candidates, hset, temp, target);
+        SolveSum(0, candidates, target, duplicates, temp);
 
-        vector<vector<int>> ans(hset.begin(), hset.end());
+        set<vector<int>> single(duplicates.begin(),duplicates.end());
+        vector<vector<int>>ans(single.begin(), single.end());
         return ans;
     }
 };
